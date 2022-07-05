5bd2135e4b9ea41efddb19e44c7892669409557d
AlikhanNurkamal <nurkamalalikhan@gmail.com>
2022-07-05 16:45:17 +0500

    slight changes in the code

--- udemy_course/10_k_nearest_neighbors/knn_classifier.py
+++ udemy_course/10_k_nearest_neighbors/knn_classifier.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
